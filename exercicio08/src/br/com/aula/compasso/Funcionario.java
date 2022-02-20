fbf56a2005ffd42ae74e84d91eda58326496e548
Otavio de Mello <otaviodemello15@gmail.com>
2022-02-20 18:59:11 -0300

    Melhorias de identação e organização do Exercicio 08

--- exercicio08/src/br/com/aula/compasso/Funcionario.java
+++ exercicio08/src/br/com/aula/compasso/Funcionario.java
@@ -5,21 +5,24 @@ import java.util.ArrayList;
 public class Funcionario {
 
     String nome;
-    double salario;
-    static double salarioLiquido;
-    static float porcentagem;
+    private double salario;
+    private static double salarioLiquido;
+    private static float porcentagem;
     private static ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
 
+    //Contrutor de Funcionarios.
     Funcionario(String nome, double salario){
         this.nome = nome;
         this.salario = salario;
     }
 
+    //Método para adicionar um novo funcionário ao ArrayList, recebe nome e salário informados pelo usuário.
     static void cadastraFuncionario(String inputNome, double inputSalario){
         funcionarios.add(new Funcionario(inputNome, inputSalario));
     }
 
-    static double validaSalario(){
+    //Método que passa pelo ArrayList e faz a validação do valor de salário seguindo a regra de negócio.
+    static void validaSalario(){
         for (int i = 0; i < funcionarios.size(); i++) {
 
             double salarioAtual = funcionarios.get(i).salario;
@@ -28,30 +31,29 @@ public class Funcionario {
                 salarioLiquido = salarioAtual + (salarioAtual * porcentagem);
                 System.out.println("====================================");
                 System.out.println("Nome: " + funcionarios.get(i).nome);
-                System.out.printf("Salário: %,.2f %n", salarioAtual);
-                System.out.printf("Bonus: %,.2f %n", salarioAtual * porcentagem);
-                System.out.printf("Salário Liquido: %,.2f %n", salarioLiquido);
+                System.out.printf("Salário: R$%,.2f %n", salarioAtual);
+                System.out.printf("Bonus: R$%,.2f %n", salarioAtual * porcentagem);
+                System.out.printf("Salário Liquido: R$%,.2f %n", salarioLiquido);
                 System.out.println("====================================");
             }else if(salarioAtual >= 1000 && salarioAtual < 2000){
                 porcentagem = 0.10f;
                 salarioLiquido = salarioAtual + (salarioAtual * porcentagem);
                 System.out.println("====================================");
                 System.out.println("Nome: " + funcionarios.get(i).nome);
-                System.out.printf("Salário: %,.2f %n", salarioAtual);
-                System.out.printf("Bonus: %,.2f %n", salarioAtual * porcentagem);
-                System.out.printf("Salário Liquido: %,.2f %n", salarioLiquido);
+                System.out.printf("Salário: R$%,.2f %n", salarioAtual);
+                System.out.printf("Bonus: R$%,.2f %n", salarioAtual * porcentagem);
+                System.out.printf("Salário Liquido: R$%,.2f %n", salarioLiquido);
                 System.out.println("====================================");
             }else{
                 porcentagem = 0.10f;
                 salarioLiquido = salarioAtual - (salarioAtual * porcentagem);
                 System.out.println("====================================");
                 System.out.println("Nome: " + funcionarios.get(i).nome);
-                System.out.printf("Salário: %,.2f %n", salarioAtual);
-                System.out.printf("Desconto: %,.2f %n", salarioAtual * porcentagem);
-                System.out.printf("Salário Liquido: %,.2f %n", salarioLiquido);
+                System.out.printf("Salário: R$%,.2f %n", salarioAtual);
+                System.out.printf("Desconto: R$%,.2f %n", salarioAtual * porcentagem);
+                System.out.printf("Salário Liquido: R$%,.2f %n", salarioLiquido);
                 System.out.println("====================================");
             }
         }
-        return salarioLiquido;
     }
 }
--- exercicio08/src/br/com/aula/compasso/Main.java
+++ exercicio08/src/br/com/aula/compasso/Main.java
@@ -6,18 +6,28 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //boolean responsável por inicializar o menu e manter ele rodando.
         boolean menu = true;
         Scanner scanner = new Scanner(System.in);
 
+        //Enquanto menu = true esse do será executado.
     do {
-        System.out.println("Informe a opção desejada: \n1 - Cadastrar Funcionário\n0 - Sair\n");
-        int opcaoMenu = scanner.nextInt();
+        System.out.println("============================\n" +
+                "Informe a opção desejada: " +
+                "\n1 - Cadastrar Funcionário" +
+                "\n0 - Sair\n" +
+                "============================\n");
+        String opcaoMenu = scanner.nextLine();
 
         switch (opcaoMenu){
-            case 1:
+            case "1":
+
+                //Pega a quantidade de funcionários que o usuário deseja cadastrar e salva na váriavel quantidadeDeFuncionarios
+                // (sem tratamento para 0 funcionários).
                 System.out.println("Infome a quantidade de funcionários: ");
                 int quantidadeDeFuncionarios = scanner.nextInt();
 
+                //for responsável por pegar o nome e salário dos funcionários.
                 for (int i = 0; i < quantidadeDeFuncionarios; i++) {
                     scanner.nextLine();
                     System.out.println("Informe o nome do(a) " + (i+1) + "° funcionário(a): ");
@@ -25,19 +35,25 @@ public class Main {
                     System.out.println("Informe o salário: ");
                     double inputSalario = scanner.nextDouble();
 
+                    //Chamada do método cadastraFuncionario, recebendo o nome e salário informados pelo usuário.
                     Funcionario.cadastraFuncionario(inputNome, inputSalario);
                 }
 
+                //Chamada do método validaSalario, que irá aplicar a regra de negócio e imprimir os resultados.
                Funcionario.validaSalario();
 
                 break;
 
-            case 0:
+            case "0":
+
+                //Caso seja selecionada a opção 0 - Sair, será exibida a mensagem a seguir e a variavel menu receberá false, saindo do looping.
                 System.out.println("Finalizando operação!");
                 menu = false;
                 break;
 
             default:
+
+                //Caso nenhuma opção válida seja selecionada a variavel menu receberá false, o processo será abortado.
                 System.out.println("Operação não encontrada!\nAbortando operação");
                 menu = false;
                 break;
